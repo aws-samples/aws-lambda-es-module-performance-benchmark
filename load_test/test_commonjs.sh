a736b5cd0366c8ed358c2695899a9cea4d89ad10
Daniel Fox <dfxamzn@amazon.com>
2022-01-05 12:44:03 -0700

    add Press any key to continue

--- load_test/test_commonjs.sh
+++ load_test/test_commonjs.sh
@@ -2,4 +2,4 @@
 
 $(npm bin)/artillery run --config config.yaml scenarios/commonjs.yaml
 
-
+read -n 1 -s -r -p "Press any key to continue"
--- load_test/test_esmodule.sh
+++ load_test/test_esmodule.sh
@@ -2,4 +2,4 @@
 
 $(npm bin)/artillery run --config config.yaml scenarios/esmodule.yaml
 
-
+read -n 1 -s -r -p "Press any key to continue"
